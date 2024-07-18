76fe441a868cb7779c24182aba34f6e95b5954d4
Udaydhingra7 <udaydhingra7@gmail.com>
2024-07-18 17:42:58 +0530

    new

---
+++ HW.py
@@ -0,0 +1,9 @@
+Marks=int(input("Enter the marks obtained:"))
+if Marks>90:
+    print("Grade: A")
+elif 80<Marks:
+    print("Grade: B")
+elif 60<=Marks:
+    print("Grade: C")
+else:
+    print("Grade: D")
