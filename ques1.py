0cbc65dcf84776c8cdc2abe4a1665ce2f3c9f74b
Udaydhingra7 <udaydhingra7@gmail.com>
2024-07-26 16:43:39 +0530

    n

--- ques1.py
+++ ques1.py
@@ -1,3 +1,4 @@
 def intro(name, age):
-    print("Hi, My name is "+name+" and i'm "+str(age)+" year old")
+    print("Hi, My name is "+name+" and i'm "+str(age)+" years old.")
+
 intro("sharma ji ka ladka",20)
